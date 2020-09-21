a14d941e53cd33ada1c822966858ed614632b52a
tuimac <tuimac.devadm01@gmail.com>
2020-09-21 12:25:07 +0000

    add new

---
+++ initOpenVPN.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from pexpect import *
+import os
+import subprocess
+
+if __name__ == '__main__':
+    easyrsaDir = '/usr/share/easy-rsa/3'
+    passwd = 'password'
+
+    os.chdir(easyrsaDir)
+    #subprocess.run(['./easyrsa', 'init-pki'])
+    child = spawn('./easyrsa build-ca')
+    child.expect('Enter*:')
+    child.sendline(passwd)
+    child.expect('Re-Enter*:')
+    child.sendline(passwd)
+    child.expect('Common Name*:')
+    child.sendline('tuimac')
