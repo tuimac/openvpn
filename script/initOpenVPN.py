ec63d4e821a2267431ca324575a097a7ec43591a
tuimac <tuimac.devadm01@gmail.com>
2020-10-04 07:09:18 +0000

    fixed program structures

--- script/initOpenVPN.py
+++ script/initOpenVPN.py
@@ -2,18 +2,43 @@
 
 from pexpect import *
 import os
+import stat
 import subprocess
+import traceback
+
+def checkTunnelDevice():
+    try:
+        deviceDir = '/dev/net'
+        mode = 0o644 | stat.S_IFCHR
+        if not os.path.exists(deviceDir):
+            os.mkdir(deviceDir)
+            os.mknod(deviceDir + '/tun', mode=mode, device=200)
+    except Exception as e:
+        raise e
+
+def initOpenVpn():
+    subprocess.run(['./easyrsa', 'init-pki'])
+
+def buildCa():
+    try:
+        child = spawn('./easyrsa build-ca')
+        child.expect('Enter*')
+        child.sendline(passwd)
+        child.expect('Re-Enter*')
+        child.sendline(passwd)
+        child.expect('Common Name*')
+        child.sendline('tuimac')
+    except Exception as e:
+        raise e
 
 if __name__ == '__main__':
     easyrsaDir = '/usr/share/easy-rsa/3'
     passwd = 'password'
 
-    os.chdir(easyrsaDir)
-    subprocess.run(['./easyrsa', 'init-pki'])
-    child = spawn('./easyrsa build-ca')
-    child.expect('Enter*')
-    child.sendline(passwd)
-    child.expect('Re-Enter*')
-    child.sendline(passwd)
-    child.expect('Common Name*')
-    child.sendline('tuimac')
+    try:
+        checkTunnelDevice()
+        os.chdir(easyrsaDir)
+        initOpenVpn()
+        buildCa()
+    except:
+        traceback.print_exc()
