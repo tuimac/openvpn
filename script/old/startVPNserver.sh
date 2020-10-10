022d4fd1e77ed049a40c32b62decf514337b1b62
tuimac <tuimac.devadm01@gmail.com>
2020-10-10 07:59:11 +0000

    fixed

--- script/old/startVPNserver.sh
+++ script/old/startVPNserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openvpn --config /etc/openvpn/server.conf --status /etc/openvpn/openvpn-server/status.log --status-version 2 --suppress-timestamps --cipher AES-256-GCM --ncp-ciphers AES-256-GCM:AES-128-GCM:AES-256-CBC:AES-128-CBC:BF-CBC
+openvpn --config /etc/openvpn/server.conf
