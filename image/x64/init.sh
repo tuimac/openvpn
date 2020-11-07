120aa512649853160a4f74fecf9be382244cd63a
EC2 Default User <ec2-user@docker.tuimac.private>
2020-11-07 07:16:40 +0000

    add travis ci

---
+++ image/x64/init.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+CLIENTCERTPATH='/etc/openvpn/'${CLIENTCERTNAME}'.ovpn'
+EASYRSA='/usr/share/easy-rsa'
+SERVERCONF='/etc/openvpn/server.conf'
+INITFLAG='/etc/openvpn/.initflag'
+
+function generateCert(){
+    cd $EASYRSA
+    ./easyrsa init-pki
+    ./easyrsa --batch build-ca nopass
+    ./easyrsa gen-dh
+    openvpn --genkey --secret /etc/openvpn/ta.key
+    ./easyrsa build-server-full server nopass
+    ./easyrsa build-client-full tuimac nopass
+}
+
+function convertNetmask(){
+    local network=${1}
+    local index=0
+    RESULT=''
+
+    for x in ${network//// }; do
+        [[ $index -eq 0 ]] && { RESULT=$x' '; }
+        [[ $index -eq 1 ]] && { SUBNET=$x; }
+        ((index++))
+    done
+    local subnetmask=''
+    for((i=0; i < $((SUBNET / 8)); i++)); do
+        subnetmask+='255.'
+    done
+    subnetmask+=$((256 - ( 1 << (8 - (SUBNET % 8)))))
+    for((i=0; i < $((3 - (SUBNET / 8))); i++)); do
+        subnetmask=${subnetmask}'.0'
+    done
+    RESULT+=$subnetmask
+}
+
+function createClientCert(){
+    cat <<EOF > $CLIENTCERTPATH
+client
+dev tun
+proto udp
+remote $PUBLICIP $EXTERNALPORT
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+user nobody
+group nobody
+remote-cert-tls server
+tls-client
+comp-lzo
+cipher AES-256-CBC
+verb 4
+tun-mtu 1500
+key-direction 1
+EOF
+
+    echo '<ca>' >> $CLIENTCERTPATH
+    cat ${EASYRSA}/pki/ca.crt >> $CLIENTCERTPATH
+    echo '</ca>' >> $CLIENTCERTPATH
+
+    echo '<key>' >> $CLIENTCERTPATH
+    cat ${EASYRSA}/pki/private/${CLIENTCERTNAME}.key >> $CLIENTCERTPATH
+    echo '</key>' >> $CLIENTCERTPATH
+
+    echo '<cert>' >> $CLIENTCERTPATH
+    cat ${EASYRSA}/pki/issued/${CLIENTCERTNAME}.crt >> $CLIENTCERTPATH
+    echo '</cert>' >> $CLIENTCERTPATH
+
+    echo '<tls-auth>' >> $CLIENTCERTPATH
+    cat /etc/openvpn/ta.key >> $CLIENTCERTPATH
+    echo '</tls-auth>' >> $CLIENTCERTPATH
+}
+
+function downloadPem(){
+    python3 /etc/openvpn/pem.py $CLIENTCERTPATH $INTERNALPORT
+}
+
+function serverConfig(){
+    cat <<EOF > $SERVERCONF
+port ${INTERNALPORT}
+proto udp
+dev tun
+ca /usr/share/easy-rsa/pki/ca.crt
+cert /usr/share/easy-rsa/pki/issued/server.crt
+key /usr/share/easy-rsa/pki/private/server.key
+dh /usr/share/easy-rsa/pki/dh.pem
+keepalive 10 120
+tls-auth /etc/openvpn/ta.key 0 # This file is secret
+cipher AES-256-CBC
+comp-lzo
+max-clients 1
+user nobody
+group nobody
+persist-key
+persist-tun
+status /var/log/openvpn-status.log
+log         /var/log/openvpn.log
+log-append  /var/log/openvpn.log
+verb 4
+explicit-exit-notify 1
+EOF
+    convertNetmask $VIRTUALNETWORK
+    echo 'server '${RESULT} >> $SERVERCONF
+    env | grep -E 'ROUTING[[:digit:]]' | while read line; do
+        local index=0
+        for part in ${line//=/ }; do
+            if [ $index -eq 1 ]; then
+                convertNetmask $part
+                echo 'push "route '${RESULT}'"' >> $SERVERCONF
+            fi
+            ((index++))
+        done
+    done
+}
+
+function startVPN(){
+    mkdir /dev/net
+    mknod /dev/net/tun c 10 200
+    iptables -t nat -A POSTROUTING -s $VIRTUALNETWORK -o eth0 -j MASQUERADE
+    env | grep -E 'ROUTING[[:digit:]]' | while read line; do
+        local index=0
+        for part in ${line//=/ }; do
+            if [ $index -eq 1 ]; then
+                iptables -t nat -A POSTROUTING -s $part -o eth0 -j MASQUERADE
+                break
+            fi
+            ((index++))
+        done
+    done
+    exec openvpn --config /etc/openvpn/server.conf
+}
+
+function main(){
+    if [[ ! -e $INITFLAG ]]; then
+        touch $INITFLAG
+        generateCert
+        serverConfig
+        createClientCert
+        downloadPem
+    else
+        echo 'Installation of OpenVPN already done.'
+    fi
+    startVPN
+    if [ $? -eq 0 ]; then
+        echo 'Starting OpenVPN process has been sucessed.'
+    else
+        echo 'Starting OpenVPN process has been failed.'
+    fi
+}
+
+main
