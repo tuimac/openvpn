60c071584341a4500adad87445097590f047bcc3
EC2 Default User <ec2-user@docker.tuimac.private>
2020-11-10 12:03:22 +0000

    Easier to use

--- images/run.sh
+++ images/run.sh
@@ -4,8 +4,8 @@
 ##############################
 NAME="openvpn"
 VOLUME="${PWD}/volume"
-DOCKERHUBUSER="tuimac"
-IMAGE=${DOCKERHUBUSER}/${NAME}
+DOCKERHUBUSER=""
+CPUARCH="x64"
 ##############################
 
 function runContainer(){
@@ -29,9 +29,9 @@ function cleanup(){
 
 function createContainer(){
     mkdir ${VOLUME}
-    docker build -t ${NAME} -f Dockerfile-x64 .
+    docker build -t ${NAME} -f Dockerfile-${CPUARCH} .
     runContainer
-    #cleanup
+    cleanup
 }
 
 function rerunContainer(){
@@ -61,43 +61,6 @@ function commitImage(){
     docker start ${NAME}
 }
 
-function pushImage(){
-    commitImage ${IMAGE}
-    docker push ${IMAGE}
-    if [ $? -ne 0 ]; then
-        cat .password.txt | base64 -d | docker login --username ${DOCKERHUBUSER} --password-stdin
-        if [ $? -ne 0 ]; then
-            docker login --username ${DOCKERHUBUSER}
-        fi
-        docker push ${IMAGE}
-    fi
-    docker rmi ${IMAGE}
-    cleanup
-}
-
-function registerSecret(){
-    local secretFile=".password.txt"
-    if [ -e $secretFile ]; then
-        echo -en "There is '.password.txt' file in your current directory."
-        echo -en "Continue this? [y/n]: "
-        read answer
-        if [ $answer == "n" ]; then
-            echo "Registering password is skipped."
-            exit 0
-        elif [ $answer == "y" ]; then
-            echo "" > /dev/null
-        else
-            echo "Only type in 'y' or 'n'."
-            exit 1
-        fi
-    fi
-    echo -en "Password: "
-    read -s password
-    echo
-    chmod 600 ${secretFile}
-    echo $password | base64 > ${secretFile}
-    chmod 400 ${secretFile}
-}
 
 function userguide(){
     echo -e "usage: ./run.sh [help | create | delete | commit | register-secret]"
@@ -107,8 +70,6 @@ create              Create image and container after that run the container.
 rerun               Delete only container and rerun container with new settings.
 delete              Delete image and container.
 commit              Create image from target container and push the image to remote repository.
-push                Push image you create to Docker Hub.
-register-secret     Create password.txt for make it login process within 'commit' operation.
     "
 }
 
@@ -122,12 +83,8 @@ function main(){
         deleteAll
     elif [ $1 == "commit" ]; then
         commitImage ${NAME}
-    elif [ $1 == "push" ]; then
-        pushImage
     elif [ $1 == "help" ]; then
         userguide
-    elif [ $1 == "register-secret" ]; then
-        registerSecret
     else
         { userguide; exit 1; }
     fi
